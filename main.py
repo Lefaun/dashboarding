d7d4221fad127731d20fda5600642af3b29f7c6d
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-10 16:41:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,42 @@ with fig_col1:
     chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["LAT", "LON", "Freguesia"])
     
     st.area_chart(chart_data)
+with fig_col2:
+
+import pydeck as pdk
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=38.75,
+        longitude=-9.02,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
 #
    # st.title = "Location ID"
    # st.markdown("Location ID")
@@ -77,12 +113,12 @@ with fig_col1:
 
 
 
-with fig_col2:
-    st.markdown("Tree Map")
-    fig = px.density_heatmap(
-        data_frame=df, y="ID", x="Freguesia"
-    )
-    st.write(fig)
+#with fig_col2:
+  #  st.markdown("Tree Map")
+  #  fig = px.density_heatmap(
+   #     data_frame=df, y="ID", x="Freguesia"
+   # )
+   # st.write(fig)
 
 
 
