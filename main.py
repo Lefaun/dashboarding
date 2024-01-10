e1fdab886bda202683f1ffe5f7b9a3c7706d5d49
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-10 15:08:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,7 @@ with fig_col2:
 st.title = "Road Map of Data Science"
 st.markdown("road map")
 
-df = pd.read_csv('arte-urbana - arte_urbana_fev2022.csv', 
-                  usecols=['Freguesia', 'LONG', 'LAT'])
+df = pd.read_csv('arte-urbana - arte_urbana_fev2022.csv', usecols=['Freguesia', 'LONG', 'LAT'])
 #df = pd.DataFrame(df.columns = ['Freguesia', 'LONG', 'LAT'])
 st.map(df)
 
