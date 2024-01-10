bd71e6ec4fd1e3fd17acc876292e39c99c9a23ea
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-10 15:11:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -90,9 +90,14 @@ st.markdown("road map")
 
 
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 df = pd.DataFrame(
-np.random.randn(1000,2) / [50, 50] + [-86, 39],
-columns=['Freguesia','LONG', 'LAT',])
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['Freguesia''LAT', 'Long'])
+
 st.map(df)
 
 st.markdown("Detailed Data View")
